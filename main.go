52a84c924b1af67a85e7ee5350abaf09ffac3233
Dmitrii <d.mingalev99@gmail.com>
2021-11-13 06:32:49 +0300

    Add port value in env

--- main.go
+++ main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 	"github.com/nftid/nftid-node/router"
+	"log"
 )
 
 func main() {
 	app := fiber.New()
 
+	envs, err := godotenv.Read()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	router.SetupRoutes(app)
 
-	app.Listen(":7535")
+	app.Listen(":" + envs["PORT"])
 }
